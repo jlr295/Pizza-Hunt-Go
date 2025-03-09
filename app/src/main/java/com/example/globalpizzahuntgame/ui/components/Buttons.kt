65c67f1433724a529a57d2082ebadb6c1e7902ca
Jaclyn Rutter <jaclynrutter19@gmail.com>
2025-03-09 12:41:09 -0400

    Submit Button Started

--- app/src/main/java/com/example/globalpizzahuntgame/ui/components/Buttons.kt
+++ app/src/main/java/com/example/globalpizzahuntgame/ui/components/Buttons.kt
@@ -1,2 +1,13 @@
 package com.example.globalpizzahuntgame.ui.components
 
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SubmitButton(){
+
+    Button(
+
+    )
+}
